5dbab0f253633ba8e40ed06646e1dfc9f63a3087
João Paulo Rechi Vita <jprvita@gmail.com>
2021-03-25 13:39:48 -0700

    Add script to build Ardour on Mac OS
    This downloads ardour-build-tools and ardour repos to a temporary workdir,
    runs 'build-stack' to download, build and install all dependencies to a
    path in the user's homedir, builds Ardour using the correct dependencies,
    and package it as a DMG file.
    
    At the end of the process it cleans-up all build artifacts, leaving only
    the DMG file on the directory where the script was started from.

---
+++ build-ardour-macos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -ex
+
+# Server to use when cloning ardour repositories
+GIT_SERVER=git://git.ardour.org/ardour
+
+
+WORKDIR=$(mktemp -d -t $(basename $0))
+SRC_CACHE=/var/tmp/src_cache/
+
+# Fetch and build dependencies
+BUILD_TOOLS_SRC_DIR=${WORKDIR}/ardour-build-tools
+BUILD_TOOLS_REPO=${GIT_SERVER}/ardour-build-tools.git
+git clone ${BUILD_TOOLS_REPO} ${BUILD_TOOLS_SRC_DIR}
+mkdir -p ${BUILD_TOOLS_SRC_DIR}/build
+pushd ${BUILD_TOOLS_SRC_DIR}/build
+${BUILD_TOOLS_SRC_DIR}/build-stack
+popd
+rm -rf ${BUILD_TOOLS_SRC_DIR}
+
+# Fetch, build and package Ardour
+STACK_PREFIX=$HOME/gtk/inst
+ARDOUR_SRC_DIR=${WORKDIR}/ardour
+ARDOUR_REPO=${GIT_SERVER}/ardour.git
+git clone ${ARDOUR_REPO} ${ARDOUR_SRC_DIR}
+pushd ${ARDOUR_SRC_DIR}
+ARDOUR_VERSION=$(git describe --abbrev=0)
+git checkout ${ARDOUR_VERSION}
+touch ${ARDOUR_SRC_DIR}/libs/ardour/revision.cc
+./waf configure --optimize \
+  PKG_CONFIG_PATH=${STACK_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH \
+  PATH=${STACK_PREFIX}/bin:$PATH
+./waf
+pushd tools/osx_packaging
+./osx_build --public
+popd
+popd
+mv ${ARDOUR_SRC_DIR}/tools/osx_packaging/Ardour-${ARDOUR_VERSION}.0.dmg .
+rm -rf ${ARDOUR_SRC_DIR}
+rm -rf ${STACK_PREFIX}
+rm -rf ${WORKDIR}
+rm -rf ${SRC_CACHE}
+echo "Ardour package built at $(ls Ardour-${ARDOUR_VERSION}.0.dmg)"
